a93c2c06703b2d3d081d3fb59d022da4a788efeb
zubair2001 <zubair_mansuri2001@outlook.com>
2022-12-01 22:59:52 +0500

    About.js edited

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import React from 'react'
 const About = () => {
 
     return (
-        <div>About</div>
+        <div>
+            <div style={{"display":"flex", "justifyItems":"center","alignItems":"center", "fontSize":"40px"}}>This is iNotebook App. Designed to store notes.<br/> It is made with the help of React and NodeJS.</div>
+        </div>
     )
 }
 
